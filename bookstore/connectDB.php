d5b09f892352a334995cc19a053baf49c621faef
dunduoffice <153175610+dunduoffice@users.noreply.github.com>
2023-12-07 21:58:20 +0530

    Update connectDB.php

--- bookstore/connectDB.php
+++ bookstore/connectDB.php
@@ -1,5 +1,5 @@
 <?php
 $con = mysqli_init();
 mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-mysqli_real_connect($conn, "dunduwebapp1-server.mysql.database.azure.com", "root", "{Dundappa@123}", "{bookstore}", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "dunduwebapp1-server.mysql.database.azure.com", "root", "Dundappa@123", "bookstore", 3306, MYSQLI_CLIENT_SSL);
 ?>
