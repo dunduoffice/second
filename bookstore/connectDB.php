8028f35f0516a182e0d9217f0637446fa0b88c53
dunduoffice <153175610+dunduoffice@users.noreply.github.com>
2023-12-07 16:57:18 +0530

    Update connectDB.php

--- bookstore/connectDB.php
+++ bookstore/connectDB.php
@@ -1,3 +1,5 @@
 <?php
-$pdo=new PDO('mysql:host=localhost;port=3306;dbname=bookstore','root', 'Dundappa@123');
+$con = mysqli_init();
+mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
+mysqli_real_connect($conn, "dunduwebapp1-server.mysql.database.azure.com", "root", "{Dundappa@123}", "{bookstore}", 3306, MYSQLI_CLIENT_SSL);
 ?>
